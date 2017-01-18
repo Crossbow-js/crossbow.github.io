14952012c90e3fa6b9d7fd1e016a398e9ca3599a
Shane Osbourne <shane.osbourne8@gmail.com>
2017-01-18 17:31:46 +0000

    Add basic tab nav

--- _src/snippets/hero.js
+++ _src/snippets/hero.js
@@ -1,27 +1,14 @@
-const cb  = require('crossbow');
-const vfs = require('vinyl-fs');
-
-// Reuse existing gulp-* plugins...
-cb.task('sass', () =>
-    vfs.src('app/scss')
-        .pipe(sass())
-        .pipe(vfs.dest('dist/css'))
-);
-
-// Combine with NPM scripts (minus the wrapppers)
-cb.task('build', [
-    '@npm rimraf ./dist',
-    'sass',
-    '@npm webpack'
-]);
-
-// NO special syntax for sequential execution
-cb.task('html', ['clean', 'build-templates']);
-
-// EASIER to compose series / parallel queues
-cb.task('default', [
-    'styles',
-    ['lint', 'html', 'scripts', 'images', 'copy'],
-    'generate-service-worker'
-]);
-
+module.exports = {
+    build: ['styles', 'scripts'],
+    scripts: [
+        '@sh rm -rf ./dist',
+        '@npm webpack --optimize'
+    ],
+    styles: {
+        tasks: 'sass-task.js',
+        options: {
+            input: 'app/scss',
+            output: 'dist/css'
+        }
+    }
+};
